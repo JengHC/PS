86b8191a2f31247e0939bdbe1a6dc9178ef0b690
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-03-21 18:57:27 +0900

    2588

---
+++ 2588/2588.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream> //입출력 헤더파일
+using namespace std;
+
+int main()
+{
+    int A, B; // 472 , 385 
+
+    //cin >> (입력) cout << (출력)
+    cin >> A >> B;
+
+    cout << A * (B % 10) << endl; // 385에서 일의 자리(5) 반환
+    cout << A * ((B % 100) / 10) << endl;// 385에서 십의 자리(8) 반환
+    cout << A * (B / 100) << endl; // // 385에서 백의 자리(3) 반환
+    cout << A * B << endl;
+
+    return 0;
+}
