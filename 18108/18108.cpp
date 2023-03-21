6adf5744410139ecbe6e21e4e557cf6d8f126367
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-03-21 18:34:06 +0900

    18108

--- 18108/18108.cpp
+++ 18108/18108.cpp
@@ -3,19 +3,12 @@ using namespace std;
 
 int main()
 {
-    int A;
-    int B;
-    int C;
+    int year;
 
     //cin >> (입력) cout << (출력)
-    cin >> A;
-    cin >> B;
-    cin >> C;
+    cin >> year;
 
-    cout << (A + B) % C << endl;
-    cout << ((A % C) + (B % C)) % C << endl;
-    cout << (A * B) % C << endl;
-    cout << ((A % C) * (B % C)) % C << endl;
+    cout << year-543 << endl;
 
     return 0;
 }
