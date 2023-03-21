a1fc1415b49abb99aa702f50e2b321c67ce12e19
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-03-21 18:03:20 +0900

    1000

--- 1000/1000.cpp
+++ 1000/1000.cpp
@@ -1,7 +1,18 @@
-﻿#include <iostream>
+﻿#include <iostream> //입출력 헤더파일
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int A ;
+    int B ;
+
+    //cin >> (입력) cout << (출력)
+    cin >> A;
+    cin >> B;
+
+    cout << A+B << endl;
+
+    return 0;
 }
 
