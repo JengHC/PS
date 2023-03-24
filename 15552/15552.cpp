d122d90df3c4f31a171e7081c172ac5c6da5557d
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-03-24 20:12:56 +0900

    15552

---
+++ 15552/15552.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int A, B, T;
+	cin >> T;
+
+	for (int i = 0; i < T; i++)
+	{
+		cin >> A >> B;
+		cout << A + B << "\n";
+	}
+}
