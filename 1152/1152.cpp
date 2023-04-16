9494063da2ef3bfb95e9f57c4a0fe27326fe3da8
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-04-16 22:38:06 +0900

    1152

--- 1152/1152.cpp
+++ 1152/1152.cpp
@@ -8,7 +8,7 @@ int main() {
 	getline(cin, name);
 
 	count = 1;
-	//빈칸이 존재하면 하나씩 증가
+	// 빈칸이 존재하면 하나씩 증가한다.
 	for (int i = 0; i < name.length(); i++) 
 	{
 		if (name[i] == ' ') 
@@ -16,10 +16,12 @@ int main() {
 			count++;
 		}
 	}
+	// 처음 시작 
 	if (name[0] == ' ')
 	{
 		count--;
 	}
+	// 마지막
 	if (name[name.length() - 1] == ' ')
 	{
 		count--;
